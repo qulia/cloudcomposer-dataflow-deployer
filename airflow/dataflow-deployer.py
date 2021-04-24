7b7c862bad837767adb9ab90e81f77f1fab89e85
Hulya Pamukcu Crowell <hulyapac@Hulyas-MBP.home>
2021-04-23 20:18:26 -0700

    Add checks

--- airflow/dataflow-deployer.py
+++ airflow/dataflow-deployer.py
@@ -3,7 +3,7 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.utils.dates import days_ago
 from airflow.operators import PythonOperator
 from airflow.models import Variable
-
+from typing import Callable, Dict, List
 from operators.dataflow_ext import (
     DataflowTemplatedJobStopOperator
 )
@@ -12,6 +12,15 @@ from airflow.providers.google.cloud.operators.dataflow import (
     CheckJobRunning,
     DataflowTemplatedJobStartOperator,
 )
+
+from airflow.providers.google.cloud.sensors.dataflow import (
+    DataflowJobAutoScalingEventsSensor,
+    DataflowJobMessagesSensor,
+    DataflowJobMetricsSensor,
+    DataflowJobStatusSensor,
+)
+
+from airflow.exceptions import AirflowException
 
 with models.DAG(
     "gcp_dataflow_template_runner",
@@ -58,6 +67,66 @@ with models.DAG(
         location="{{ dag_run.conf['location'] }}",
     )
 
+    # https://github.com/apache/airflow/blob/master/airflow/providers/google/cloud/example_dags/example_dataflow.py
+    # [START howto_sensor_wait_for_job_metric]
+    def check_metric_scalar_gte(metric_name: str, value: int) -> Callable:
+        """Check is metric greater than equals to given value."""
+
+        def callback(metrics: List[Dict]) -> bool:
+            dag_template.log.info("Looking for '%s' >= %d", metric_name, value)
+            for metric in metrics:
+                context = metric.get("name", {}).get("context", {})
+                original_name = context.get("original_name", "")
+                tentative = context.get("tentative", "")
+                if original_name == "Service-cpu_num_seconds" and not tentative:
+                    return metric["scalar"] >= value
+            raise AirflowException(f"Metric '{metric_name}' not found in metrics")
+
+        return callback
+
+    wait_for_job_metric = DataflowJobMetricsSensor(
+        task_id="wait-for-job-metric",
+        job_id="{{task_instance.xcom_pull('start-template-job')['id']}}",
+        location="{{ dag_run.conf['location'] }}",
+        callback=check_metric_scalar_gte(metric_name="Service-cpu_num_seconds", value=100),
+    )
+    # [END howto_sensor_wait_for_job_metric]
+
+    # [START howto_sensor_wait_for_job_message]
+    def check_message(messages: List[dict]) -> bool:
+        """Check message"""
+        for message in messages:
+            if "Adding workflow start and stop steps." in message.get("messageText", ""):
+                return True
+        return False
+
+    wait_for_job_message = DataflowJobMessagesSensor(
+        task_id="wait-for-job-message",
+        job_id="{{task_instance.xcom_pull('start-template-job')['id']}}",
+        location="{{ dag_run.conf['location'] }}",
+        callback=check_message,
+    )
+    # [END howto_sensor_wait_for_job_message]
+
+    # [START howto_sensor_wait_for_job_autoscaling_event]
+    def check_autoscaling_event(autoscaling_events: List[dict]) -> bool:
+        """Check autoscaling event"""
+        for autoscaling_event in autoscaling_events:
+            if "Worker pool started." in autoscaling_event.get("description", {}).get("messageText", ""):
+                return True
+        return False
+
+    wait_for_job_autoscaling_event = DataflowJobAutoScalingEventsSensor(
+        task_id="wait-for-job-autoscaling-event",
+        job_id="{{task_instance.xcom_pull('start-template-job')['id']}}",
+        location="{{ dag_run.conf['location'] }}",
+        callback=check_autoscaling_event,
+    )
+    # [END howto_sensor_wait_for_job_autoscaling_event]
+
     print_content >> stop_job
     print_options >> stop_job
     stop_job >> start_job
+    start_job >> wait_for_job_metric
+    start_job >> wait_for_job_message
+    start_job >> wait_for_job_autoscaling_event
